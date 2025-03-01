b56503f58dd97b721e2db7f405fb52185d6e3be7
Radleigh123 <103168649+radleigh123@users.noreply.github.com>
2025-03-01 10:54:52 +0800

    files init

--- app/src/main/java/com/example/vscan/MainActivity.kt
+++ app/src/main/java/com/example/vscan/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.vscan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Simulates a splash screen
+        var i = 3
+        val textView: TextView = findViewById(R.id.tv_test1)
+        lifecycleScope.launch{
+            while (i > 0) {
+                i.toString().also { textView.text = it }
+                delay(500)
+                i--
+            }
+            startActivity(Intent(this@MainActivity, LoginActivity2::class.java))
+            finish()
         }
     }
 }
